1887f9cfb0605e398cf0f62d54edbe7a11645a88
rafaeltorrese <torresc.rafael@gmail.com>
2022-01-25 22:18:48 -0600

    function for english words

--- profitable-app/basics.py
+++ profitable-app/basics.py
@@ -1,17 +1,46 @@
+# %%
 import csv
 import pathlib
+# %%
 
 
-data_path_google = pathlib.Path('profitable-app/data/googleplaystore.csv')
-data_path_apple = pathlib.Path('profitable-app/data/AppleStore.csv')
+def explore_data(dataset, start, end, rows_and_columns=False):
+    dataset_slice = dataset[start:end]
+    for row in dataset_slice:
+        print(f'ID: {start}. NumFields {len(row)}')
+        print(row)
+        print('\n')
+        start += 1
+
+    if rows_and_columns:
+        print(f'Number of rows: {len(dataset)}')
+        print(f'Number of columns: {len(dataset[0])}')
+
+
+def english_app(word):
+    return all(ord(letter) < 128 for letter in word)
+
+
+def english_app2(word, threshold=3):
+    number_non_english = sum(ord(letter) > 127 for letter in word)
+    return number_non_english < threshold
+
+
+# %%
+data_path_google = pathlib.Path('data/googleplaystore.csv')
+data_path_apple = pathlib.Path('data/AppleStore.csv')
 # F: \slides\data-science-lecture\profitable-app\data\AppleStore.csv
+# %%
 
-with open(data_path_google, 'r', encoding='utf-8', newline='') as f:
-    data_google = [row for row in csv.reader(f)]
+if __name__ == '__main__':
+    with open(data_path_google, 'r', encoding='utf-8', newline='') as f:
+        header_google = list(next(csv.reader(f)))
+        data_google = [*csv.reader(f)]
 
-with open(data_path_apple, 'r', encoding='utf-8', newline='') as f:
-    data_apple = [row for row in csv.reader(f)]
+    with open(data_path_apple, 'r', encoding='utf-8', newline='') as f:
+        header_apple = [*next(csv.reader(f))]
+        data_apple = [*csv.reader(f)]
 
-print(data_google[0])
-print()
-print(data_apple[0])
+    explore_data(data_google, 10470, 10475)
+    # print(header_google.index('Price'))
+    # print(header_google.index('Type'))
