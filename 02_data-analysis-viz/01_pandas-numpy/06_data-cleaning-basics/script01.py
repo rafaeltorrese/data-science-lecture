2b69bd58eeabbeadf155fbc1df0e155f2956eb31
rafaeltorrese <torresc.rafael@gmail.com>
2022-02-17 05:44:19 -0600

    dataframe info

--- 02_data-analysis-viz/01_pandas-numpy/06_data-cleaning-basics/script01.py
+++ 02_data-analysis-viz/01_pandas-numpy/06_data-cleaning-basics/script01.py
@@ -1,10 +1,10 @@
-#%%
+# %%
 import numpy as np
 import pandas as pd
-#%%
+# %%
 # laptops = pd.read_csv('laptops.csv')  # error
 # print(laptops) # error
 
 laptops = pd.read_csv('laptops.csv', encoding='Latin-1')
 # print(laptops.info())
-#%%
+# %%
